ce327250cf0adc2ab128e46bc331024f42b432cc
AceKiron <mojanghouse@gmail.com>
2021-07-10 12:02:42 +0200

    Added going through pictures and facts

---
+++ main.js
@@ -0,0 +1,48 @@
+let pictures, facts;
+
+let picturesIndex, factsIndex;
+
+let pictureElem, factElem;
+
+function setPicture(i) {
+	while (i < 0) { i += pictures.length; }
+	while (i >= pictures.length) { i -= pictures.length; }
+
+	pictureElem.attr("src", pictures[i]);
+}
+
+function setFact(i) {
+	while (i < 0) { i += facts.length; }
+	while (i >= facts.length) { i -= facts.length; }
+
+	factElem.html(facts[i]);
+}
+
+$(document).ready(async function() {
+	pictures = await getPictures();
+	facts = await getFacts();
+
+	pictureElem = $("#axolotl-api-picture");
+	factElem = $("#axolotl-api-fact");
+
+	setPicture(picturesIndex = Math.floor(Math.random() * pictures.length));
+	setFact(factsIndex = Math.floor(Math.random() * facts.length));
+
+	$("button").click(function() {
+		if ($(this).hasClass("axolotl-api-picture")) {
+			// Go through pictures
+			if ($(this).hasClass("next")) {
+				setPicture(++picturesIndex);
+			} else {
+				setPicture(--picturesIndex);
+			}
+		} else if ($(this).hasClass("axolotl-api-fact")) {
+			// Go through facts
+			if ($(this).hasClass("next")) {
+				setFact(++factsIndex);
+			} else {
+				setFact(--factsIndex);
+			}
+		}
+	});
+});
