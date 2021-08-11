69c88cf319206f283447e0e4d289a021e1368aa0
AceKiron <mojanghouse@gmail.com>
2021-08-11 20:51:01 +0200

    Made main.js redirect to the page that should be checked

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 if (window.location.hostname != "theaxolotlapi.netlify.app") {
+	if (window.location.pathname != "/axolotlbasics.html" && window.location.protocol != "file:") {
+		window.location.href = "axolotlbasics.html";
+	}
+
 	$(document).ready(function() {
 		$("<h5>Please keep in mind this is the development branch, content on this page does not represent the actual page.<br>To visit the actual website, <a href=\"https://theaxolotlapi.netlify.app\" class=\"btn\">click here</a></h5>").insertBefore("main");
 	});
